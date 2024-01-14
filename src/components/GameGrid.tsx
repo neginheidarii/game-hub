e71e9d67b4d98f8496dd2f5ed316005526219369
Negin Heidari <neginhd6@gmail.com>
2024-01-14 14:55:16 -0500

    building game card and grid. making it responsive

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,23 @@
-import {SimpleGrid, Text} from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 
-
 const GameGrid = () => {
-    const {games, error} = useGames();
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error} </Text>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} spacing={10} padding='10px'>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />        ))}
+          <GameCard key={game.id} game={game} />
+        ))}
       </SimpleGrid>
     </>
   );
-}
+};
 
-export default GameGrid
+export default GameGrid;
