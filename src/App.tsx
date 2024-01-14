693d9a33ff2ea8ce925a7774c45dc56e36e64e4e
Negin Heidari <neginhd6@gmail.com>
2024-01-14 00:31:11 -0500

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral" h="100px">
+        Nav
+      </GridItem>
+      {/*  Show component is used to show/hide components based on breakpoints */}
+      <Show above="lg">
+        <GridItem area="aside" bg="gold" h="100px">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue" h="100px">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
